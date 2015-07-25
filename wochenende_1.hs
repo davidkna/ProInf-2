60a4ccb137c8582e863be7f8de2898666adf7195
David Knaack <david@knaack.me>
2015-07-25 16:10:05 +0200

    nextCollatz 2

--- wochenende_1.hs
+++ wochenende_1.hs
@@ -42,10 +42,11 @@ echtTeiler n = [x | x <- [1 .. (n `div` 2)], n `mod` x == 0]
 -- Aufgabe 5
 
 -- Aufgabe 6
+-- 6a)
+nextCollatz :: Integer -> Integer
+nextCollatz n | n `mod` 2 == 0 = n `div 2
+			  | otherwise      = n * 3 + 1
 
-nextCollatz :: Factorial a => a -> a
-nextCollatz n | n `mod` 2 == 0 = n / 2
-			  | otherwise      = n * 3 + 1 
 -- Aufgabe 7
 {-Hilfe für 7.
 
