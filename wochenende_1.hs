ce405bb5fc722b71632fade3dd5b825f40231d49
Scuide <espoint2@yahoo.de>
2015-07-25 14:44:13 +0200

    Material aus der Stunde
    Alles was wir im Tutorium dazu hatten

--- wochenende_1.hs
+++ wochenende_1.hs
@@ -8,6 +8,9 @@ quadSolutions :: Int -> Int -> Int -> [Int]
 quadSolutions a b c = [x | x <- [minBound :: Int .. maxBound :: Int], a * x^2 + b * x + c == 0]
 -- Ansatz 2 mit pq Formel
 
+quadSolutions :: Int -> Int -> Int -> [Int]
+quadSolutions a b c = [ x | x <- [(-10000),(-9999) .. 10000], (a*x^2 + b*x + c) == 0]
+
 -- Aufgabe 2
 
 -- Aufgabe 3
@@ -19,7 +22,29 @@ quadSolutions a b c = [x | x <- [minBound :: Int .. maxBound :: Int], a * x^2 + 
 -- Aufgabe 6
 
 -- Aufgabe 7
+{-Hilfe für 7.
 
+01010
+10101
+01010
+10101
+01010
+
+foo::Int->Int->Int->Char
+foo x y s = toEnum(48+(x+y)`mod`2))
+
+001100
+001100
+110011
+110011
+001100
+
+
+foo::Int->Int->Int->Char
+foo x y s = toEnum(48+((x`div`w+y`div`w)`mod`2)
+	where w = s`div`8
+
+	-}
 -- Vorgegebene Funktion
 paintPicture :: ((Int, Int, Int) -> Char) -> Int -> [Char]
 paintPicture f size = paint size (map f [(x,y,size) | x <- [1..size], y <- [1..size]])
