7776c226a3b8beae8a7ff2fe4dd1217994cc4f13
David Knaack <david@knaack.me>
2015-07-25 14:08:34 +0200

    Füge vorgegebene Funktion hinzu

--- wochenende_1.hs
+++ wochenende_1.hs
@@ -3,9 +3,23 @@
 -- David Knaack und Evan Seilz
 
 -- Aufgabe 1
+
 -- Aufgabe 2
+
 -- Aufgabe 3
+
 -- Aufgabe 4
+
 -- Aufgabe 5
+
 -- Aufgabe 6
+
 -- Aufgabe 7
+
+-- Vorgegebene Funktion
+paintPicture :: ((Int, Int, Int) -> Char) -> Int -> [Char]
+paintPicture f size = paint size (map f [(x,y,size) | x <- [1..size], y <- [1..size]])
+                      where
+                        paint 0  []     = []
+                        paint 0 (c:cs)  = '\n' : (paint size (c:cs))
+                        paint n (c:cs)  = c: (paint (n-1) cs)
