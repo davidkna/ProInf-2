d6e56e07cfa215e413b2c1cdc325faf8ee6cce69
Scuide <espoint2@yahoo.de>
2015-07-27 15:57:52 +0200

    Mitschrift im Tutorium

--- u7.hs
+++ u7.hs
@@ -1,3 +1,10 @@
+{-
+Übungsblatt 7 27.07.15
+Evan Seilz David Knaack
+-}
+--1.--
+
+{- 1. Ansatz
 selectSort :: (Ord a) => (a -> a -> Bool) -> [a] -> [a]
 selectSort operator liste = [getFirst liste]
 	where
@@ -9,3 +16,61 @@ selectSort operator liste = [getFirst liste]
 			| x == y = ys
 			| otherwise = x : (delFirst x ys)
 		delFirst _ _ = undefined
+		-}
+--2. Ansatz
+
+selectSort::(Ord a)=>(a->Bool)->[a]->[a]
+selectSort f xs = helpSort xs [] f
+	where
+		helpSort[] xs _ = xs
+		helpSort xs ys f = helpSort(delFirst xs f)((getFirst xs f):ys) f
+		getFirst [] _ x = x
+		getFirst (x:xs) f y
+			|f x y = getFirst xs f y
+			|otherwise = getFirst xs f x
+		delFirst [] _ _ = []
+		delFirst [_] _ _ = []
+		delFirst (x:xs)
+			|getFirst(x:xs) f x ==x =xs
+			|otherwise = x:(delFirst xs f)
+
+
+
+--2.--
+--a)i
+
+tails::[a]->[[a]]
+tails [] = []
+tails (x:xs) = (x:xs):tails xs
+
+--a)ii
+
+prefix::Eq a => [a]->[a]->[a]
+prefix [] _ = []
+prefix _ [] = []
+prefix (x:xs)(y:ys)
+	| x==y=x prefix xs ys
+	| otherwise []
+
+--a)iii
+
+longestPrefix xs =sel(helper xs) 
+	where
+		helper xs | length xs < 2 = (0,[])
+		helper (x:y:xs) = (length p,p):helper (y:xs)
+			where
+				p=prefix x y 
+sel [x]=x 
+sel ((l1,s1):(l2,s2):xs)
+	| l1>=l2=sel((l1,s1):xs)
+	|otherwise = sel ((l2,s2):xs)
+
+longestRepSeq:: Eq a =>[a]->[a]
+longestRepSeq xs = snd(longestPrefix(sort(<=)(tails xs)))
+{- 3. 
+Tails hat eine Komplexität von T(n)= O(n)
+sort hat eine Komplexität von T(n)=O(n log n)*T<= =O(n²log n)
+Prefix hat eine Komplexität von T(n)= O(n)
+LongestPrefix hat eine Komplexität von T(n)=O(n²)
+snd hat eine Komplexität von O(1)
+}
