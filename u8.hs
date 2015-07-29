5c879ac708c17d9173b43c2898655180655b3c3d
David Knaack <david@knaack.me>
2015-07-29 19:40:59 +0200

    updated

--- u8.hs
+++ u8.hs
@@ -4,7 +4,7 @@ Evan Seilz David Knaack
 -}
 --1.--
 data Gewicht = Kilo Double | Pfund Double
-	deriving (Show)
+    deriving (Show)
 
 kilo2pfund::Gewicht->Gewicht
 kilo2pfund (Kilo n) = Pfund (n*2.20462262)
@@ -18,24 +18,24 @@ pfund2kilo _ = error "Input has to be Pfund"
 
 --2
 data Complex = C Int Int
-	deriving Show
+    deriving Show
 cadd :: Complex -> Complex -> Complex
-cadd (C a b) (C x y) = (C (a + x) (b + y))
+cadd (C a b) (C x y) = C (a + x) (b + y)
 
 cmultiply :: Complex -> Complex -> Complex
-cmultiply (C a b) (C x y) = (C (a * x - b * y) (b * x + a * y))
+cmultiply (C a b) (C x y) = C (a * x - b * y) (b * x + a * y)
 
 --4.--
 
 data Nat = Zero |S Nat
-	deriving Show
+    deriving Show
 
 add :: Nat->Nat->Nat
 add a Zero = a
 add a (S b) = add (S a) b
 
 nsucc :: Nat -> Nat
-nsucc n = (S n)
+nsucc = S
 
 npred :: Nat->Nat
 npred Zero = Zero
@@ -50,7 +50,7 @@ npow _ Zero = S Zero
 npow a(S b) = nmult a (npow a b)
 
 factorial :: Nat->Nat
-factorial Zero = (S Zero)
+factorial Zero = S Zero
 factorial (S a) = nmult (S a) (factorial a)
 
 lowTh :: Nat->Nat->Bool
@@ -70,7 +70,7 @@ comp (S a) (S b) = comp a b
 
 
 data Zint = Z Nat Nat
-	deriving Show
+    deriving Show
 zadd:: Zint->Zint->Zint
 zadd (Z a b) (Z c d) =Z (add a c) (add b d)
 
@@ -89,35 +89,44 @@ zsimplify(Z(S a)(S b))= zsimplify(Z a b)
 -- 4a
 ungerade :: Nat -> Bool
 ungerade Zero = False
-ungerade (S a) = not (ungerade a) 
+ungerade (S a) = not (ungerade a)
 
--- a - b => 0, wenn b>=a 
+-- a - b => 0, wenn b>=a
 cutSub :: Nat -> Nat -> Nat
 cutSub Zero _      = Zero
 cutSub a    Zero   = a
-cutSub (S a) (S b) = cutSub a b 
+cutSub (S a) (S b) = cutSub a b
 
 nmin :: Nat -> Nat -> Nat
 nmin a b = if a `greatTh` b then a else b
 
 -- 4b
-nat2Int :: Nat -> Integer
+nat2Int :: Nat -> Int
 nat2Int Zero = 0
 nat2Int (S a) = 1 + nat2Int a
 
-int2Nat :: Integer -> Nat
+int2Nat :: Int -> Nat
 int2Nat 0 = Zero
 int2Nat a = S (int2Nat (a - 1))
 
 -- c
 zneg :: Zint -> Zint
 zneg a = zneg' $ zsimplify a
-	where zneg' (Z a b) = (Z b a)
+    where zneg' (Z a b) = Z b a
 
 zabs :: Zint -> Zint
-zabs (Z _ x) = (Z 0 x)
+zabs x = zabs' (zsimplify x)
+    where
+        zabs' (Z x Zero) = Z Zero x
+        zabs' z          = z
 
 zpow :: Zint -> Nat -> Zint
-zpow _ Zero = (Z (Zero (S(Zero))))
+zpow _ Zero = Z Zero (S Zero)
+zpow (Z a b) (S p) = zmult (Z a b) (zpow (Z a b) p)
 
-zggt:: Zint -> Zint	->Zint
+zint2Int :: Zint -> Int
+zint2Int (Z a b) = nat2Int b - nat2Int a
+
+int2Zint :: Int -> Zint
+int2Zint x | x > 0 = Z Zero (int2Nat x)
+           | otherwise = Z (int2Nat x) Zero
