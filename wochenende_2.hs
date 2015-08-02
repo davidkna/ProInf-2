c4963eedf323399de9aae459a52c69d804ba72be
David Knaack <david@knaack.me>
2015-08-02 23:16:13 +0200

    x

--- wochenende_2.hs
+++ wochenende_2.hs
@@ -8,7 +8,8 @@
 potenzNoRep :: Int -> Int -> [Int]
 potenzNoRep n m = mergeSort [potenzList b m | b <- [2 .. n]]
   where
-    -- Gibt Liste mit allen Potenzen zu Basis b zurück
+    -- gibt Liste mit allen Potenzen zu Basis b zurück
+    -- jede potenzList ist von klein nach groß sortiert
     potenzList b m = [b ^ e | e <- [2 .. m]]
 
     -- mergeSort bis es nur noch eine List gibt
@@ -16,7 +17,8 @@ potenzNoRep n m = mergeSort [potenzList b m | b <- [2 .. n]]
     mergeSort xs = mergeSort (mergeHelp xs)
     mergeHelp (x1:x2:xs) = merge x1 x2 : mergeHelp xs
     mergeHelp xs = xs
-    -- merge entfert Duplikate
+
+    -- merge ist verändert zum Entfernen von Duplikate
     merge xs [] = xs
     merge [] ys = ys
     merge (x:xs) (y:ys) = case x `compare` y of
@@ -74,17 +76,42 @@ enqueue (Q a b) x = Q a (x:b)
 -- Gibt Tupel mit neuer Queue und Element zurück
 dequeue :: Queue a -> (Queue a, a)
 dequeue (Q [] []) = error "Empty Queue"
-dequeue (Q [] xs) =
-    let rxs = reverse xs
-    in (Q rxs [], head rxs)
+dequeue (Q [] ys) = dequeue $ Q (reverse ys) []
 dequeue (Q (x:xs) ys) = (Q xs ys, x)
 
- -- Aufgabe 4
- {-
-  1) korrekt   Variable Z
-  2) inkorrekt
-  3) inkorrekt
-  4) inkorrekt
-  5) korrekt keine Variable
-  6) inkorrekt
--}
+-- Aufgabe 4
+--     1) inkorrekt
+--     2) inkorrekt
+--     3) korrekt - y, z und w sind frei, Rest nicht frei
+--     4) inkorrekt
+--     5) korrekt - x, b und c sind frei, Rest nicht frei
+--     6) korrekt - w und b sind frei
+
+-- Aufgabe 5
+-- 1.
+--     (λx.xx) (λabc.cbabc) (λx.fxx)(fx)y
+--     (λabc.cbabc) (λabc.cbabc) (λx.fxx)(fx)y
+--     (λx.fxx) (fx) (λabc.cbabc) (fx) (λx.fxx)y
+--     f(fx)(fx) y (λx.fxx) (fx) (λx.fxx) y
+--     f(fx)(fx) y f(fx)(fx) (λx.fxx) y
+--     ffxfxyffxfxfyy
+
+-- 2.
+--     (λx.x)((λabc.b(abc))(λfx.f(fx))))
+--     (λt.t)((λabc.b(abc))(λfx.f(fx)))
+--     (λabc.b(abc))(λfx.f(fx))
+--     (λbc.b((λfx.f(fx))bc))
+--     (λbc.b((λfx.f(fx))bc))
+--     (λbc.bb(bc))
+--     (λbc.bbbc)
+
+-- Aufgabe 6
+--     λwyx. y (w y x)  ≡S
+--     (S (S λsx.s(x)))
+--     (S (S λsq.s(q)))
+--     (S (λwyx. y (w y x) λsq.s(1)))
+--     (S (λyx. y (λsq.s(q) y x) ))
+--     (S (λyx. yyx))
+--     (S (λyx. ppq))
+--     (λwyx. y (w y x)(λyx. ppq))
+--     (λyx. yyyx)
